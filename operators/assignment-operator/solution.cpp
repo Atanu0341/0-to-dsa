f92bd0d01fd877cc0417238a0858751d6cfddc02
Atanu Majumder <atanumajumder2004@gmail.com>
2024-09-14 14:26:30 +0530

    Lecture 10: Operators in C++ with implementation

---
+++ operators/assignment-operator/solution.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    // Assignment operator
+    int a = 10;
+    a+=15;
+    cout<<a<<endl;  
+
+    int b = 10;
+    b-=5;
+    cout<<b<<endl;
+
+    int c = 10;
+    c*=5;
+    cout<<c<<endl;
+    
+    
+    return 0;
+}
---
+++ operators/basic-operators/solution.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    
+    // cout<<13/2<<endl;
+    // cout<<13.7/2<<endl;
+    // cout<<13%2<<endl;
+    // cout<<4*13.7/2<<endl;
+    // cout<<4*13.2<<endl;
+
+    // int a = 10;
+    // cout<<a++<<endl;
+    // cout<<a<<endl;
+
+    // int a = 10;
+    // int b=a++;
+    // cout<<a<<" "<<b<<endl;
+
+    // int a = 10;
+    // int b=++a;
+    // cout<<a<<" "<<b<<endl;
+
+    int a = 10;
+    cout<<a--<<endl;
+    cout<<a<<endl;
+
+    // int a = 10;
+    // int b=a--;
+    // cout<<a<<" "<<b<<endl;
+
+    // int a = 10;
+    // int b=--a;
+    // cout<<a<<" "<<b<<endl;
+
+    return 0;
+}
